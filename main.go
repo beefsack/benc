180d3551186d36412b5b880000f13f682963d967
Michael Alexander <beefsack@gmail.com>
2015-03-03 17:15:44 +1100

    Removed compress option, can just gzip when piping in if wanted.

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"bufio"
-	"bytes"
-	"compress/gzip"
 	"flag"
 	"fmt"
 	"io"
@@ -13,26 +11,12 @@ import (
 
 func main() {
 	var (
-		compress             bool
 		varName, packageName string
 	)
 
 	flag.StringVar(&varName, "var", "bencoded", "name of variable to encode to")
 	flag.StringVar(&packageName, "pkg", "main", "name of package")
-	flag.BoolVar(&compress, "compress", false, "compress with gzip before encoding")
 	flag.Parse()
-
-	r := io.Reader(bufio.NewReader(os.Stdin))
-	if compress {
-		b := bytes.NewBuffer(make([]byte, 1024))
-		w := gzip.NewWriter(b)
-		go func(w io.Writer, r io.Reader) {
-			if _, err := io.Copy(w, r); err != nil {
-				log.Panicf("Error compressing: %v", err)
-			}
-		}(w, r)
-		r = b
-	}
 
 	fmt.Printf(`package %s
 
@@ -40,6 +24,7 @@ var %s = []byte{`, packageName, varName)
 
 	first := true
 	reading := true
+	r := bufio.NewReader(os.Stdin)
 	p := make([]byte, 1024)
 	for reading {
 		n, err := r.Read(p)
